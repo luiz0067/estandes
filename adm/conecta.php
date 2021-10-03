ce51d3ef5380456e23268ef875f4df105888fd69
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-10-03 12:09:57 -0300

    Add files via upload

---
+++ adm/conecta.php
@@ -0,0 +1,18 @@
+﻿<?php
+$host		=	"mysql17.redehost.com.br";
+	$login		=	"oficialestandes";
+	$password	=	"&TR*r62o";
+	$base_dados	=	"oficialestandes";
+
+if (!$link = mysql_connect($host, $login, $password)) {
+    echo 'Não foi possível conectar ao mysql';
+    exit;
+}
+
+if (!mysql_select_db($base_dados, $link)) {
+    echo 'Não foi possível selecionar o banco de dados';
+    exit;
+}
+
+
+?>
